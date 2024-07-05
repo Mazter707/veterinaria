ecd282ddd9ea3f950d45b0a272fa8a24773bdb90
JesusGarciaMtz <mazter707@gmail.com>
2024-07-04 18:54:19 -0600

    widget deleted

--- app/Providers/Filament/YPanelProvider.php
+++ app/Providers/Filament/YPanelProvider.php
@@ -24,8 +24,8 @@ class YPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
-            ->id('y')
-            ->path('y')
+            ->id('admin')
+            ->path('admin')
             ->login()
             ->colors([
                 'primary' => Color::Amber,
@@ -38,7 +38,6 @@ class YPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
